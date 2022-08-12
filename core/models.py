b69e1c42143aafa4b8dab09d0ab080efce658929
shumwe <bravinshumwe@gmail.com>
2022-08-12 22:47:13 +0300

    code: Mailing list model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MailingList(models.Model):
+    subscriber_name = models.CharField(max_length=65)
+    subscriber_email = models.EmailField()
+    joined = models.DateTimeField(auto_now=True)
+    stop = models.BooleanField(default=False, help_text="stop receiving our emails")
+    
+    def __str__(self):
+        return f"{self.subscriber_name}"
